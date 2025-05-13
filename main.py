b6c56d2080b7bf846140d55d079c4877b12a6a4f
Mikus Rodrigo Viļķins <Mikus-Rodrigo.Vilkins@edu.rtu.lv>
2025-05-13 14:45:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-
+import openpyxl
+from datetime import datetime
+Iepirkumu_saraksts = {}
+def pievienot_iepirkuma_sarakstam:
+    Produkta_nosaukums = input("Produkta nosaukums: "), produkta_kategorija = input("Produkta kategorija: "), produkta_cena = decimal(input("Cena(EUR): "))
+    Iepirkuma_saraksts[Produkta_nosaukums]={"Produkta kategorija": produkta_kategorija,
+                                            "Produkta cena(EUR)": produkta_cena
+                                            "Pirkuma statuss": "Vajag pirkt"}            
+    print(Produkta_nosaukums, "pievienots iepirkuma sarakstam.")
+def dzest_produktu_no_saraksta():
+  Produkta_nosaukums = input("Ievadiet produkta nosaukumu, kuru vajag izdzēst: ")
+  if Produkta_nosaukums in Iepirkumu_saraksts:
+    del Iepirkumu_saraksts[Produkta_nosaukums]
+    print(Produkta_nosaukums, "ir izdzēsts no saraksta\n")
+  else:
+    print("Produkts nav atrasts iepirkumu sarakstā.")
